5b4a8189afe19157bd8ba49eab5eaf5441fb2dcb
Martin Reyes <martinreyes@MacBook-Pro.local>
2022-03-31 15:49:14 -0300

    Api Rest

--- backend/install.sh
+++ backend/install.sh
@@ -1,15 +1,12 @@
 command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is not installed.  Aborting."; exit 1; }
 
-dpkg-query -l python3-venv > /dev/null 2>&1 || { echo >&2 "python3-venv is not installed. Aborting.";exit 1;}
+if [ echo$a == 'ID_LIKE=debian' ];then
+	dpkg-query -l python3-venv > /dev/null 2>&1 || { echo >&2 "python3-venv is not installed. Aborting.";exit 1;}
+	python3-venv -m .
+else
+	virtualenv .
+fi
 
-
-# if [ echo$a == 'ID_LIKE=debian' ];then
-# 	python3-venv -m .
-# else
-# 	virtualenv .
-# fi
-
-python3-venv -m .
 source bin/activate
 pip install --upgrade pip
 pip3 install -r requirements.txt

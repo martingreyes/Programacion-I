fd52f316caf74adb93ecdbbec3321a8f31368e7c
luismaSE <lm.sarmiento@alumno.um.edu.ar>
2022-03-31 16:17:34 -0300

    correcion 2

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,7 @@
 command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is not installed.  Aborting."; exit 1; }
 
+a=$(sed -n 3p /etc/os-release)
+
 if [ echo$a == 'ID_LIKE=debian' ];then
 	dpkg-query -l python3-venv > /dev/null 2>&1 || { echo >&2 "python3-venv is not installed. Aborting.";exit 1;}
 	python3-venv -m .
